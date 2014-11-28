0b232289e0585637398f88200a172f9f24928dfe
Tiago <persei7@gmail.com>
2014-11-28 21:41:06 +1100

    First commit

---
+++ class1.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# Find the time and value of max load for each of the regions
+# COAST, EAST, FAR_WEST, NORTH, NORTH_C, SOUTHERN, SOUTH_C, WEST
+# and write the result out in a csv file, using pipe character | as the delimiter.
+# An example output can be seen in the "example.csv" file.
+
+import xlrd
+import os
+import csv
+from zipfile import ZipFile
+
+datafile = "2013_ERCOT_Hourly_Load_Data.xls"
+outfile = "2013_Max_Loads.csv"
+
+
+def open_zip(datafile):
+    with ZipFile('{0}.zip'.format(datafile), 'r') as myzip:
+        myzip.extractall()
+
+
+def parse_file(datafile):
+    workbook = xlrd.open_workbook(datafile)
+    sheet = workbook.sheet_by_index(0)
+    data = []
+    # YOUR CODE HERE
+    # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
+    # Excel date to Python tuple of (year, month, day, hour, minute, second)
+    data.append("Station|Year|Month|Day|Hour|Max Load")
+    for col in range(1,sheet.ncols):
+        cv = sheet.col_values(col, start_rowx=1)
+        maxvalue = max(cv)
+        maxpos = cv.index(maxvalue) + 1
+        maxtime = sheet.cell_value(maxpos, 0)
+        realtime = xlrd.xldate_as_tuple(maxtime, 0)
+        col_name = sheet.cell_value(0,col)
+        data.append(col_name + "|" + "|".join([str(x) for x in realtime]) + "|" + str(round(maxvalue,1)))
+    for i in data:
+        print i
+    return data
+
+def save_file(data, filename):
+    #data2 = ["2|3|4","1|4|5"]
+    #filename2 = "anything.csv"
+    with open(filename, "w") as f:
+        w = csv.writer(f, delimiter = ",")
+        w.writerow(data)
+    
+def test():
+    #open_zip(datafile)
+    data = parse_file(datafile)
+    save_file(data, outfile)
+
+    number_of_rows = 0
+    stations = []
+
+    ans = {'FAR_WEST': {'Max Load': '2281.2722140000024',
+                        'Year': '2013',
+                        'Month': '6',
+                        'Day': '26',
+                        'Hour': '17'}}
+    correct_stations = ['COAST', 'EAST', 'FAR_WEST', 'NORTH',
+                        'NORTH_C', 'SOUTHERN', 'SOUTH_C', 'WEST']
+    fields = ['Year', 'Month', 'Day', 'Hour', 'Max Load']
+
+    with open(outfile) as of:
+        csvfile = csv.DictReader(of, delimiter="|")
+        for line in csvfile:
+            station = line['Station']
+            if station == 'FAR_WEST':
+                for field in fields:
+                    # Check if 'Max Load' is within .1 of answer
+                    if field == 'Max Load':
+                        max_answer = round(float(ans[station][field]), 1)
+                        max_line = round(float(line[field]), 1)
+                        assert max_answer == max_line
+
+                    # Otherwise check for equality
+                    else:
+                        assert ans[station][field] == line[field]
+
+            number_of_rows += 1
+            stations.append(station)
+
+        # Output should be 8 lines not including header
+        assert number_of_rows == 8
+
+        # Check Station Names
+        assert set(stations) == set(correct_stations)
+
+        
+if __name__ == "__main__":
+    test()
